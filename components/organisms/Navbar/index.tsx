4dbdf1a91a96dba6f6a7404b031c6718430e2b6d
Dicka Ismaji <dickaismaji@gmail.com>
2022-02-04 16:53:30 +0700

    [Feat] dynamic active nav menu

--- components/organisms/Navbar/index.tsx
+++ components/organisms/Navbar/index.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import Menu from './Menu';
 import Auth from './Auth';
 import ToggleMenu from './ToggleMenu';
 
 export default function Navbar() {
+  const { asPath } = useRouter();
   return (
     <section>
       <nav className="navbar navbar-expand-lg navbar-light bg-light bg-white pt-lg-40 pb-lg-40 pt-30 pb-50">
@@ -14,12 +16,12 @@ export default function Navbar() {
           <ToggleMenu />
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ms-auto text-lg gap-lg-0 gap-2">
-              <Menu title="Home" active />
-              <Menu title="Games" href="/games" active={false} />
-              <Menu title="Rewards" href="/rewards" active={false} />
-              <Menu title="Discover" href="/discover" active={false} />
-              <Menu title="Global Rank" href="/global-rank" active={false} />
-              <Auth isLogin />
+              <Menu title="Home" active={asPath === '/'} />
+              <Menu title="Games" href="/games" active={asPath === '/games'} />
+              <Menu title="Rewards" href="/rewards" active={asPath === '/rewards'} />
+              <Menu title="Discover" href="/discover" active={asPath === '/discover'} />
+              <Menu title="Global Rank" href="/global-rank" active={asPath === '/global-rank'} />
+              <Auth isLogin={false} />
             </ul>
           </div>
         </div>
