88cd279aa51488bbd6bae1a567f39ca20827b033
Dicka Ismaji <dickaismaji@gmail.com>
2022-02-07 15:06:52 +0700

    [Feat] signin page

--- components/organisms/SigninForm/index.tsx
+++ components/organisms/SigninForm/index.tsx
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import Cookies from 'js-cookie';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
+import { signin } from '../../../services/auth';
 
 export default function SigninForm() {
+  const router = useRouter();
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleFormOnSubmit = async (e: any) => {
+    e.preventDefault();
+    setIsButtonDisabled(true);
+
+    // Form validation
+    if (!form.email || !form.password) {
+      toast.warning('Please fill email and password first');
+      setIsButtonDisabled(false);
+      return false;
+    }
+
+    try {
+      const result = await signin(form);
+      const { token } = result;
+
+      const tokenBase64 = window.btoa(token);
+
+      Cookies.set('token', tokenBase64, { expires: 30 });
+
+      toast.success('Sign in successful');
+      localStorage.setItem('token', result.token);
+
+      router.push('/');
+    } catch (err: any) {
+      setIsButtonDisabled(false);
+      toast.error(err.response?.data?.message);
+    }
+
+    return false;
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleFormOnSubmit}>
       <div className="container mx-auto">
         <div className="pb-50">
           <Link href="/" passHref>
@@ -31,6 +82,8 @@ export default function SigninForm() {
             name="email"
             aria-describedby="email"
             placeholder="Enter your email address"
+            value={form.email}
+            onChange={handleInputChange}
           />
         </div>
         <div className="pt-30">
@@ -47,16 +100,18 @@ export default function SigninForm() {
             name="password"
             aria-describedby="password"
             placeholder="Your password"
+            value={form.password}
+            onChange={handleInputChange}
           />
         </div>
         <div className="button-group d-flex flex-column mx-auto pt-50">
-          <a
+          <button
             className="btn btn-sign-in fw-medium text-lg text-white rounded-pill mb-16"
-            href="../index.html"
-            role="button"
+            type="submit"
+            disabled={isButtonDisabled}
           >
             Continue to Sign In
-          </a>
+          </button>
           <Link href="/signup" passHref>
             <a
               className="btn btn-sign-up fw-medium text-lg color-palette-1 rounded-pill"
