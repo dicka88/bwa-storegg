6c8632e235f081ebee9ccf374b8bef1afb7cfa17
Dicka Ismaji <dickaismaji@gmail.com>
2022-02-14 17:27:58 +0700

    [Refactor] remove unused import

--- pages/member/transactions/index.tsx
+++ pages/member/transactions/index.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import React from 'react';
 import Sidebar from '../../../components/organisms/Sidebar';
 import TransactionContent from '../../../components/organisms/TransactionContent';
